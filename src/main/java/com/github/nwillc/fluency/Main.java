3c5a313221baf8b33118323ed1343b3dec621451
Nicholas Christopher <nwillc@gmail.com>
2017-08-25 19:01:25 -0400

    logging

--- src/main/java/com/github/nwillc/fluency/Main.java
+++ src/main/java/com/github/nwillc/fluency/Main.java
@@ -5,8 +5,11 @@
 package com.github.nwillc.fluency;
 
 
+import org.pmw.tinylog.Logger;
+
 public class Main {
     public static void main(String[] args) {
+        Logger.info("Start");
         System.out.println("Hello");
     }
 }
