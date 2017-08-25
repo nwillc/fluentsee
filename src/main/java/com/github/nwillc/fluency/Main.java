c19e3c1926604958547b59685abd385f7e908ea4
Nicholas Christopher <nwillc@gmail.com>
2017-08-25 19:04:55 -0400

    cleaning

--- src/main/java/com/github/nwillc/fluency/Main.java
+++ src/main/java/com/github/nwillc/fluency/Main.java
@@ -1,15 +1,13 @@
-/*
- * foo
- */
 
 package com.github.nwillc.fluency;
 
 
 import org.pmw.tinylog.Logger;
 
-public class Main {
+public final class Main {
     public static void main(String[] args) {
         Logger.info("Start");
         System.out.println("Hello");
+        Logger.info("End");
     }
 }
