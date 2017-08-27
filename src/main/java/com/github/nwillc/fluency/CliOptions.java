f53296dbd7288070c6c1826fc886312251641d48
Nicholas Christopher <nwillc@gmail.com>
2017-08-26 23:21:31 -0400

    More cli work

--- src/main/java/com/github/nwillc/fluency/CliOptions.java
+++ src/main/java/com/github/nwillc/fluency/CliOptions.java
@@ -7,6 +7,7 @@ import joptsimple.OptionParser;
 final public class CliOptions {
     public enum CLI {
         log,
+        match,
         help
     }
 
@@ -17,7 +18,12 @@ final public class CliOptions {
         OptionParser parser = new OptionParser(true);
         parser.accepts(CLI.help.name(), "Get command line help.").forHelp();
         parser.accepts(CLI.log.name(), "Log file to use.")
-                .withRequiredArg().ofType(String.class).describedAs("filename");
+                .withRequiredArg().ofType(String.class)
+                .describedAs("filename")
+                .required();
+        parser.accepts(CLI.match.name(),"Define a match for filtering output. May pass in multiple matches.")
+                .withRequiredArg().ofType(String.class)
+                .describedAs("field=regex");
         return parser;
     }
 }
