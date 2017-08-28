fc1ea3490fa4dbb90864575480d3cd3f1aef35fb
Nicholas Christopher <nwillc@gmail.com>
2017-08-27 22:18:46 -0400

    compile predicate regex

--- src/main/java/com/github/nwillc/fluentsee/predicates/ContainerPredicate.java
+++ src/main/java/com/github/nwillc/fluentsee/predicates/ContainerPredicate.java
@@ -8,17 +8,18 @@ package com.github.nwillc.fluentsee.predicates;
 import com.github.nwillc.fluentsee.Entry;
 
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 
 public class ContainerPredicate implements Predicate<Entry> {
-    private final String regex;
+    private final Pattern regex;
 
     public ContainerPredicate(String regex) {
-        this.regex = regex;
+        this.regex = Pattern.compile(regex);
     }
 
     @Override
     public boolean test(Entry entry) {
-        return entry.container.matches(regex);
+        return regex.matcher(entry.container).matches();
     }
 }
